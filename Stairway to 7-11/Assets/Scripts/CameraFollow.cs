d2a3d8c4d731d6e24b8efd7c7a23ce7bffbaece8
Victor Hernandez <20.victor.hernandez@ksd.org>
2020-02-05 13:56:10 -0800

    test

---
+++ Stairway to 7-11/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+    public float yMoveSpeed = 0.1f;
+    void FixedUpdate ()
+    {
+        float desiredPosition = target.position.x + offset.x;
+        float smoothedPositionX = Mathf.Lerp(transform.position.x, desiredPosition, smoothSpeed);
+        transform.position = new Vector3(smoothedPositionX, transform.position.y, transform.position.z);
+    }
+}
